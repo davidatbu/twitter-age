bd5a66d7967b33bc9ef6b9f72e6f86a60a44ff89
davidatbu <davidat@bu.edu>
2020-12-10 17:32:48 -0500

    tried to make it even faster, wasn't any faster

--- extract_features_bert.py
+++ extract_features_bert.py
@@ -1,7 +1,19 @@
 from __future__ import annotations
+
+from concurrent.futures import ThreadPoolExecutor
+import os
+from dnips.iter import myzip
+from dnips.nn import pt_pack_unpack
+from functools import partial
+
+from more_itertools.more import unzip
 import numpy as np
 
+from numpy import ceil
+
 import torch
+
+from torch.tensor import Tensor
 import tqdm
 from more_itertools import chunked
 
@@ -13,88 +25,90 @@ from dnips.nn.tsfmrs import Pipeline
 import typer
 from pathlib import Path
 import pandas as pd
-from typing import List
+from typing import Iterable, Iterator, List, Sequence, Tuple
 
 logger = Logger(__name__)
 
+
+def get_last_layers_cls(texts: List[str], pipeline: Pipeline) -> torch.Tensor:
+    _, _, hidden_states = pipeline(texts)
+    # Take last four layers
+    hidden_states = hidden_states[-4:]
+
+    # Flatten the representation.
+    flat = torch.cat(hidden_states, dim=-1)
+    # Take [CLS] representation
+    cls = flat[:, 0, :]
+
+    return cls
 
 def main(
     out_dir: Path,
     users_csv: Path,
     json_dir: Path,
-    batch_size: int = 500,
+    batch_size: int = 100,
     mdl_name: str = "bert-base-uncased",
 ) -> None:
     log_file = out_dir / "bert_feature_extract.log"
     logger.addHandler(FileHandler(log_file))
     logger.addHandler(StreamHandler())
 
-    logger.info("Called with out_dir, users_csv, json_dir, batch_size")
-
-    pipeline = Pipeline(mdl_name)
-
-    expected_users = set(
-        u.strip("@").lower()
-        for u in pd.read_csv(
-            users_csv,
-            encoding="utf-8",
-            usecols=[
-                "Author",
-            ],
-            na_filter=False,
-        )["Author"].tolist()
+    logger.info(
+        f"Called with out_dir={out_dir}, users_csv={users_csv}, json_dir={json_dir}, batch_size={batch_size}"
     )
 
-    jsonl_dset = JsonlDataset(
-        list(json_dir.glob("*.jsonl")),
+    df_expected_users = pd.read_csv(users_csv, usecols=["Author"], na_filter=False)
+    expected_users = set(df_expected_users['Author'].str.strip('@').str.lower())
+
+    # Filter out empty ones
+    all_fpaths = list(json_dir.glob("*.jsonl"))
+    fpaths = []
+    with ThreadPoolExecutor(40) as executor:
+        sizes = executor.map(os.path.getsize, all_fpaths)
+        for fpath, size in tqdm.tqdm(zip(all_fpaths, sizes), desc='checking file sizes.'):
+            if size > 0:
+                 fpaths.append(fpath)
+
+    jsonl_dset: Sequence[List[str]] = JsonlDataset(  # type: ignore[assignment]
+            fpaths,
         selector=make_selector("renderedContent"),
     )
+
+    pipeline = Pipeline(mdl_name)
+    get_last_layers_cls_with_pipeline = partial(get_last_layers_cls, pipeline=pipeline)
 
     user_feats = []
     users: List[str] = []
 
-    user_tweets: List[str]
-    pbar = tqdm.tqdm(jsonl_dset, desc="users processed")
+
     with torch.no_grad():
-        for uname_in_fname, user_tweets in pbar:  # type: ignore[assignment]
-            pbar.set_description(f"{len(users)} users found.")
-            uname_in_fname = uname_in_fname.lower()
+        pbar = tqdm.tqdm(
+            pt_pack_unpack(get_last_layers_cls_with_pipeline, jsonl_dset, batch_size),
+            desc=f"users processed.",
+            total=len(fpaths),
+        )
+
+        for i, emb_per_tweet in enumerate(pbar):
+            uname_in_fname = fpaths[i].stem.lower()
             if uname_in_fname not in expected_users:
                 logger.error(
                     f"{uname_in_fname} doesn't match any expected users given in CSV."
                 )
                 continue
-            if not user_tweets:
-                logger.warning(f"Empty file found in {uname_in_fname}.jsonl")
-                continue
 
-            feats = []
-            for batch in chunked(user_tweets, batch_size):
-                _, _, hidden_states = pipeline(batch)
-                # Take last four layer
-                hidden_states = hidden_states[-4:]
 
-                # Flatten the representation.
-                flat = torch.cat(hidden_states, dim=-1)
-                # Take [CLS] representation
-                cls = flat[:, 0, :]
-                feats.append(cls)
-
-            # Concatenate along batch dim
-            all_feats = torch.cat(feats, dim=0)
-            pooled = all_feats.mean(dim=0).cpu().numpy()
+            # Concatenate in the batch dim
+            pooled = emb_per_tweet.detach().mean(dim=0).cpu()
             user_feats.append(pooled)
             users.append(uname_in_fname)
 
-    cat_user_feats = np.stack(user_feats, axis=0)
+    cat_user_feats = torch.stack(user_feats, dim=0)
     with (out_dir / "found_usernames.csv").open("w") as f:
         f.writelines(u + "\n" for u in users)
 
-    with (out_dir / "user_tweet_centroids.npy").open("wb") as fb:
-        np.save(fb, cat_user_feats)
+    with (out_dir / "user_tweet_centroids.pth").open("wb") as fb:
+        torch.save( cat_user_feats, fb)
 
 
 if __name__ == "__main__":
     typer.run(main)
-
-############## TESTS
